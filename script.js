17f898324c2dc2ab129d97478222b4dce466f1bd
Benjamin <Officialbenragland@gmail.com>
2024-02-12 22:41:34 -0500

    Turns the Notable Projects Title orange on hover

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const menuBar = document.querySelectorAll('.header-bar h3')
-
+const NotableProjects = document.querySelectorAll('#notable-projects h2')
 
 
 for ( let i = 0 ; i < menuBar.length ; i++){
     menuBar[i].addEventListener('mouseover',function(e){
         menuBar[i].style.color = "#E26310" 
+        
+    })
+}
+for ( let i = 0 ; i < NotableProjects.length ; i++){
+    NotableProjects[i].addEventListener('mouseover',function(e){
+        NotableProjects[i].style.color = "#E26310" 
         
     })
 }

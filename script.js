4b3d57050937136525fa2383d5584f1bae0ab7eb
Benjamin <Officialbenragland@gmail.com>
2024-02-12 23:15:39 -0500

    add spacing between sections

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const NotableProjects = document.querySelectorAll('#notable-projects h2')
 
 for ( let i = 0 ; i < menuBar.length ; i++){
     menuBar[i].addEventListener('mouseover',function(e){
+       
         menuBar[i].style.color = "#E26310" 
-        
     })
 }
+
 for ( let i = 0 ; i < NotableProjects.length ; i++){
     NotableProjects[i].addEventListener('mouseover',function(e){
         NotableProjects[i].style.color = "#E26310" 
